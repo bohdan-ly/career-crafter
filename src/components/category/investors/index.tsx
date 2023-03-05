5083034a5dc80f65474d649f8d0457baec141365
Bohdan <bohdan@klevercompany.com>
2023-03-05 13:47:30 +0200

    fixed carousel

--- src/components/category/investors/index.tsx
+++ src/components/category/investors/index.tsx
@@ -2,10 +2,9 @@ import { InvestorsCrawl } from "./investors-crawl";
 
 export const Investors = () => {
   return (
-    <div className="flex flex-col w-full space-y-4 pt-5">
-      <InvestorsCrawl className="transform -translate-x-full md:translate-x-0" />
-      <InvestorsCrawl className="transform translate-x-1/3 md:translate-x-0" />
-      <InvestorsCrawl className="transform -translate-x-full md:translate-x-0" />
+    <div className="flex flex-col w-full space-y-4 pt-5 overflow-hidden">
+      <InvestorsCrawl className="transform" />
+      <InvestorsCrawl className="transform" rtl />
     </div>
   );
 };
--- src/components/category/investors/investors-crawl.tsx
+++ src/components/category/investors/investors-crawl.tsx
@@ -4,9 +4,10 @@ import Slider from "react-slick";
 
 type Props = {
   className?: string;
+  rtl?: boolean;
 };
 
-export const InvestorsCrawl: React.FC<Props> = ({ className }) => {
+export const InvestorsCrawl: React.FC<Props> = ({ className, rtl = false }) => {
   const slickRef = React.useRef<Slider | null>(null);
   const settings = {
     arrows: false,
@@ -14,39 +15,39 @@ export const InvestorsCrawl: React.FC<Props> = ({ className }) => {
     infinite: true,
     autoplay: true,
     autoplaySpeed: 0,
-    speed: 6000,
-    // rtl: true,
-    slidesToShow: 6,
-    slidesToScroll: 6,
+    speed: 15000,
+    rtl: rtl,
+    slidesToShow: 5,
+    slidesToScroll: 5,
     rows: 1,
     cssEase: "linear",
-    // variableWidth: true,
+    variableWidth: true,
     slidesPerRow: 1,
   };
 
   return (
     <div className={classNames("flex items-center w-full", className)}>
       <div className="m-auto w-full flex space-x-5">
-        {/* <Slider ref={slickRef} {...settings}> */}
-        <div className="flex px-2 items-center h-10 bg-green-200 rounded-lg">
-          Investor
-        </div>
-        <div className="flex px-2 items-center h-10 bg-red-200 rounded-lg">
-          Investor
-        </div>
-        <div className="flex px-2 items-center h-10 bg-orange-200 rounded-lg">
-          Investor
-        </div>
-        <div className="flex px-2 items-center h-10 bg-yellow-200 rounded-lg">
-          Investor
-        </div>
-        <div className="flex px-2 items-center h-10 bg-gray-200 rounded-lg">
-          Investor
-        </div>
-        <div className="flex px-2 items-center h-10 bg-gray-200 rounded-lg">
-          Investor
-        </div>
-        {/* </Slider> */}
+        <Slider ref={slickRef} {...settings}>
+          <div className="flex px-2 items-center h-10 bg-green-200 rounded-lg">
+            Investor
+          </div>
+          <div className="flex px-2 items-center h-10 bg-red-200 rounded-lg">
+            Investor
+          </div>
+          <div className="flex px-2 items-center h-10 bg-orange-200 rounded-lg">
+            Investor
+          </div>
+          <div className="flex px-2 items-center h-10 bg-yellow-200 rounded-lg">
+            Investor
+          </div>
+          <div className="flex px-2 items-center h-10 bg-gray-200 rounded-lg">
+            Investor
+          </div>
+          <div className="flex px-2 items-center h-10 bg-gray-200 rounded-lg">
+            Investor
+          </div>
+        </Slider>
       </div>
     </div>
   );
